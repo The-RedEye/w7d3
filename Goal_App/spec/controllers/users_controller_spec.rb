0d47cfcee7d4f0ab3b8ff2933a178e7edfb5ae2d
Tristan Angeles <setswipe@gmail.com>
2021-09-22 15:47:47 -0400

    finished initial user rspec. testing formatting of rspec

--- Goal_App/spec/controllers/users_controller_spec.rb
+++ Goal_App/spec/controllers/users_controller_spec.rb
@@ -1,19 +1,19 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 
-RSpec.describe UserController, type: :controller do
+# RSpec.describe UserController, type: :controller do
 
-    subject(:user) {User.create(
-        username: 'jon',
-        password: 'starwars'
-    )}
+#     subject(:user) {User.create(
+#         username: 'jon',
+#         password: 'starwars'
+#     )}
 
-    describe "post #create" do 
-        it "validates username and passowrd" do 
-            post :create, params { user: {username: 'jon', passowrd: 'starwars'}} 
-            expect(response).to render_template("new")
-        end
-    end
+#     describe "post #create" do 
+#         it "validates username and passowrd" do 
+#             post :create, params { user: {username: 'jon', passowrd: 'starwars'}} 
+#             expect(response).to render_template("new")
+#         end
+#     end
 
 
-end
+# end
--- Goal_App/spec/models/user_spec.rb
+++ Goal_App/spec/models/user_spec.rb
@@ -1,27 +1,50 @@
 require 'rails_helper.rb'
 
 RSpec.describe User, type: :model do
-    subject(:user) {User.create(
+    subject(:user1) {User.create(
         username: 'jon',
-        password: 'starwars'
+        password: 'password'
     )}
 
     it { should validate_presence_of(:username)}
     it { should validate_presence_of(:password_digest)}
-    it { should validate_presence_of(:session_token)}
-    it { should validate_uniqueness_of(:username)}
+    # it { should validate_presence_of(:session_token)}
     it { should validate_length_of(:password).is_at_least(6)}
     
+    describe "uniqueness" do
+        before :each do
+            create(:user)
+        end
+        it { should validate_uniqueness_of(:username) }
+    end
 
+    describe "is_password?" do
+        let!(:user) {create(:user)}
+        context "with a valid password" do
+            it "should return true" do
+                expect(user.is_password?('password')).to be true
+            end
+        end
 
+        context "with an invalid password" do
+            it "should return false" do
+                expect(user.is_password?('starwars')).to be false
+            end
+        end
+    end
 
+    describe "password encryption" do
+        it "does not save passwords to the database" do
+            FactoryBot.create(:user, username: "stan")
+            user=User.find_by(username: "stan")
+            expect(user.password).not_to eq("password")
+        end
 
-
-
-
-
-
-
+        it "does encrypt passowrd using BCrypt" do
+            expect(BCrypt::Password).to receive(:create).with("whatever")
+            FactoryBot.build(:user, password:"whatever")
+        end
+    end
 
 
 end
